afe0a2dbedf1790502cf6e1bc6cc4562991a99b9
Charles Appiah Manu Jnr <cappiahmanu@gmail.com>
2026-06-05 13:10:36 -0400

    Configure Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Replace these placeholder values with your Firebase web app config.
 // Firebase Console -> Project settings -> General -> Your apps -> Web app.
 export const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_FIREBASE_APP_ID"
+  apiKey: "AIzaSyDUCxkQQZ-9iGH2-hjG1v7qnTQHgKDi67s",
+  authDomain: "blogme-4edc1.firebaseapp.com",
+  projectId: "blogme-4edc1",
+  storageBucket: "blogme-4edc1.firebasestorage.app",
+  messagingSenderId: "64257809517",
+  appId: "1:64257809517:web:216bd5bebe65478cd43364",
+  measurementId: "G-E04JFLW1HY"
 };
-
